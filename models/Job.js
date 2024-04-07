6d7c5970bac1f026d6e8fd42ae3824eaeb855586
varshneyansh1 <varshneyansh9@gmail.com>
2024-04-07 19:12:47 +0530

    title location unique removed

--- models/Job.js
+++ models/Job.js
@@ -2,8 +2,8 @@ const mongoose = require("mongoose");
 
 const JobSchema = new mongoose.Schema(
     {
-        title: { type: String, required: true, unique: true },
-        location: { type: String, required: true, unique: true },
+        title: { type: String, required: true},
+        location: { type: String, required: true },
         company: { type: String, required: true },
         description: { type: String, required: true },
         salary: { type: String, required: true },
