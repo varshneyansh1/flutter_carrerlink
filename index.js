429f3828ccbcc347eb10e05b59b9cf5dbf55a3ee
varshneyansh1 <varshneyansh9@gmail.com>
2024-04-09 01:21:49 +0530

    server link socket

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ const io = require('socket.io')(server,
         pingTimeout: 60000,
         cors: {
             // origin: "http:localhost:5001",
-            origin: "https://jobhubrest-production.up.railway.app",
+            origin: "https://flutter-carrerlink.onrender.com/",
         },
     });
 
